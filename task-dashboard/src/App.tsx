55c8ecf9fe1d728c7a37d9f2fb363332bfd87206
IsratErin <131592678+IsratErin@users.noreply.github.com>
2025-09-08 23:38:15 +0200

    project setup

---
+++ task-dashboard/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background text-primary">
+      <button className="px-4 py-2 rounded-lg bg-primary text-primary-foreground animate-in zoom-in">
+        Hello with Theme 🎨
+      </button>
+    </div>
+  );
+}
+
+export default App;
